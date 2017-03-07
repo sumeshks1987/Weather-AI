0086b3819dc5119ea1011f6649d53acd0c5d130a
sumeshks1987 <sumesh.ks@envigo.co.uk>
2017-03-07 13:52:40 +0530

    Update index.js

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -30,7 +30,8 @@ exports.handle = function handle(client) {
     },
 
     prompt(callback) {
-      const environment = client.getCurrentApplicationEnvironment()
+      const environment = client.getEnvironment()
+      console.log(environment)
       getCurrentWeather(environment.weatherAPIKey, client.getConversationState().weatherCity.value, resultBody => {
         if (!resultBody || resultBody.cod !== 200) {
           console.log('Error getting weather.')
